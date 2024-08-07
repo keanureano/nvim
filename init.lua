5e6ec09b52a6fc5c09aea23e4f926ec6523d7b1f
Keanu Reaño <reanokeanu@gmail.com>
2024-08-07 20:48:49 +0800

    update config

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 if vim.g.vscode then
-    -- This is the VSCode extension environment
+  -- This is the VSCode extension environment
 
-    -- Use system clipboard for copy/paste operations
-    vim.opt.clipboard:append("unnamedplus")
-
+  -- Use system clipboard for copy/paste operations
+  ---@diagnostic disable-next-line: undefined-field
+  vim.opt.clipboard:append("unnamedplus")
 else
-    -- This is the ordinary Neovim environment
+  -- This is the ordinary Neovim environment
 
-    -- Initialize lazy.nvim, LazyVim, and your plugins
-    require("config.lazy")
+  -- Initialize lazy.nvim, LazyVim, and your plugins
+  require("config.lazy")
 end
