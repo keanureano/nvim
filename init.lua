42b58a1f0076dbd9c29c02c23664e264c32e2c53
Keanu Reaño <reanokeanu@gmail.com>
2024-08-07 08:53:35 +0800

    add gitignore and license files; update neovim config with new plugin setting.

---
+++ init.lua
@@ -0,0 +1,12 @@
+if vim.g.vscode then
+    -- This is the VSCode extension environment
+
+    -- Use system clipboard for copy/paste operations
+    vim.opt.clipboard:append("unnamedplus")
+
+else
+    -- This is the ordinary Neovim environment
+
+    -- Initialize lazy.nvim, LazyVim, and your plugins
+    require("config.lazy")
+end
