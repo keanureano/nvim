f38f491c1e5910d1704f728fb4d1380a19e8a4fd
Keanu Reaño <reanokeanu@gmail.com>
2024-08-22 17:26:33 +0800

    update

--- init.lua
+++ init.lua
@@ -3,6 +3,10 @@ if vim.g.vscode then
 
   -- Use system clipboard for copy/paste operations
   vim.opt.clipboard:append("unnamedplus")
+
+  -- Rebind changing tab keys
+  vim.api.nvim_set_keymap("n", "<C-i>", "<Cmd>Tabprevious<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<C-o>", "<Cmd>Tabnext<CR>", { noremap = true, silent = true })
 else
   -- This is the ordinary Neovim environment
 
